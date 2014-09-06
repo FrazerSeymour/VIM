2d05575be08e73f33334a09d918c1a435fdc039a
Frazer Seymour <frazer@seymourandco.com>
2014-09-05 21:35:17 -0400

    Changed script - unsafe to assume all users have gvim. (Just in case anyone happens to use this. :P)

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ mkdir ./vim
 ln -s ./vimrc ~/.vimrc
 ln -s ./vim ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-gvim -c ":PluginInstall"
+vim -c ":PluginInstall"
