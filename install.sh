bd75c23000ad38dd0ba5c248c0e502816a2417fb
Frazer Seymour <frazer@seymourandco.com>
2019-10-09 18:39:35 -0500

    Remove unneeded plugin installation steps.

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ if ansible-playbook --version >/dev/null 2>&1; then
 else
     ln -s $PWD ~/.vim
     ln -s $PWD/vimrc ~/.vimrc
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim -E -u ~/.vim/plugins.vimrc +PluginInstall +qall >/dev/null 2>&1
 fi
