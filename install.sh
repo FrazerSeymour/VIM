45ff19ce0a45360ac7ca2843429f8618865eccbd
Frazer Seymour <frazer@seymourandco.com>
2019-10-10 13:12:01 -0500

    Ask for password so packages can be installed.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ansible-playbook --version >/dev/null 2>&1; then
-    ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml
+    ansible-playbook -K --connection=local --inventory 127.0.0.1, playbook.yml
 else
     ln -s $PWD ~/.vim
     ln -s $PWD/vimrc ~/.vimrc
